956c0520ebb9ddcb52a97a741d6bab551829be3d
colmfah <colmdfahey@gmail.com>
2020-11-08 22:53:40 +0000

    saving data in local storage and displaying on page

--- scripts.js
+++ scripts.js
@@ -2,14 +2,6 @@
 
 //deal with failedCalls
 
-
-
-
-
-
-
-
-
 let eu = [
   "austria",
   "belgium",
@@ -41,7 +33,7 @@ let eu = [
   "united-Kingdom",
 ];
 
-const dealWithData = (data) => {
+const dealWithData = (data, firstCall, lastCall) => {
   data.filter((e) => e.status !== 200).forEach((e) => failedCalls.push(e.url));
 
   // Manipulate data on successful calls
@@ -86,82 +78,100 @@ const dealWithData = (data) => {
       };
     });
 
-    console.log('newData', newData)
+    console.log("newData", newData);
 
-    newData.forEach(e=> {
-     
-        
-        localStorage.setItem(e.country, JSON.stringify(e.data))
+    if (firstCall) {
+      localStorage.setItem("eu", 0);
+      localStorage.setItem("countriesDownloaded", 0);
+    }
 
-        if(e.countryCode === 'ie'){
-            console.log('irl')
-            document.getElementById(e.countryCode).innerText(localStorage.getItem("ireland"))
-        }
-        
-        
-    
-    })
-       
-        
+    let currentTotal = Number(localStorage.getItem("eu"));
 
+    console.log("currentTotal from local storage", currentTotal);
 
+    newData.forEach((e) => {
+      localStorage.setItem(e.country, JSON.stringify(e.data));
 
-  
+      if (e.countryCode === "ie") {
+        let irelandObject = JSON.parse(localStorage.getItem("ireland"));
+        document.getElementById(e.countryCode).innerHTML =
+          irelandObject[irelandObject.length - 1].casesToDate;
+      }
+    });
+
+    let countriesDownloaded = Number(
+      localStorage.getItem("countriesDownloaded")
+    );
+
+    let totalCases = newData
+      .map((e) => e.data[e.data.length - 1].casesToDate)
+      .reduce((a, b) => a + b);
+
+    console.log("totalCases", totalCases);
+
+    localStorage.setItem(
+      "countriesDownloaded",
+      countriesDownloaded + newData.length
+    );
+
+    localStorage.setItem("eu", currentTotal + totalCases);
+
+    document.getElementById("downloads").innerHTML = localStorage.getItem(
+      "countriesDownloaded"
+    );
+
+    document.getElementById("euTotalCases").innerHTML = localStorage.getItem(
+      "eu"
+    );
   });
 };
 
 // Need to code so that more calls will be made if this array is not empty
 let failedCalls = [];
 
-
 // Make API call for first 9 countries
 
 const getData = () => {
+  Promise.all(
+    eu
+      .slice(0, 9)
+      .map((e) => fetch(`https://api.covid19api.com/dayone/country/${e}`))
+  ).then((firstCallResponse) => {
+    dealWithData(firstCallResponse, true);
 
-    Promise.all(
-  eu
-    .slice(0, 9)
-    .map((e) => fetch(`https://api.covid19api.com/dayone/country/${e}`))
-).then((firstCallResponse) => {
-  dealWithData(firstCallResponse);
+    // Cause dealy before next batch of countries are sent to api (to prevent too many calls error)
 
-  // Cause dealy before next batch of countries are sent to api (to prevent too many calls error)
+    setTimeout(
+      // Make API call for next 9 countries
+      () =>
+        Promise.all(
+          eu
+            .slice(9, 18)
+            .map((e) => fetch(`https://api.covid19api.com/dayone/country/${e}`))
+        ).then((secondCallResponse) => {
+          dealWithData(secondCallResponse);
 
-  setTimeout(
-    // Make API call for next 9 countries
-    () =>
-      Promise.all(
-        eu
-          .slice(9, 18)
-          .map((e) => fetch(`https://api.covid19api.com/dayone/country/${e}`))
-      ).then((secondCallResponse) => {
-        dealWithData(secondCallResponse);
+          // Cause dealy before next batch of countries are sent to api (to prevent too many calls error)
 
-        // Cause dealy before next batch of countries are sent to api (to prevent too many calls error)
+          setTimeout(
+            () =>
+              // Make API call for final 10 countries
 
-        setTimeout(
-          () =>
-          // Make API call for final 10 countries
+              Promise.all(
+                eu
+                  .slice(18)
+                  .map((e) =>
+                    fetch(`https://api.covid19api.com/dayone/country/${e}`)
+                  )
+              ).then((thirdCallResponse) => {
+                dealWithData(thirdCallResponse, false, true);
+              }),
+            5000
+          );
+        }),
+      5000
+    );
+  });
+};
 
-            Promise.all(
-              eu
-                .slice(18)
-                .map((e) =>
-                  fetch(`https://api.covid19api.com/dayone/country/${e}`)
-                )
-            ).then((thirdCallResponse) => {
-              dealWithData(thirdCallResponse);
-            }),
-          5000
-        );
-      }),
-    5000
-  );
-});
-
-
-
-}
-
-getData()
-
+getData();
