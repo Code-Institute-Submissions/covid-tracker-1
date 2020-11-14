ed0191ca2e9940999ae406001b9f62e117129847
colmfah <colmdfahey@gmail.com>
2020-11-14 17:32:46 +0000

    add tick lines

--- scripts.js
+++ scripts.js
@@ -57,30 +57,42 @@ const height = +svg.attr("height");
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
+
+
+
   const xScale = d3
     .scaleLinear()
     .domain([0, d3.max(data, xValue)])
-    .range([0, innerWidth]);
+    .range([0, innerWidth])
+ 
 
   const yScale = d3
     .scaleBand()
     .domain(data.map(yValue))
     .range([0, innerHeight])
     .padding(0.15);
+
   const g = svg
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const xAxis = d3.axisBottom(xScale).tickSize(-innerWidth)
+
+
 
   g.append("g").call(d3.axisLeft(yScale))
   .selectAll('.domain, .tick line')
   .remove()
 
-
-  g.append("g")
-    .call(d3.axisBottom(xScale))
+    const xAxisG = g.append("g")
+    .call(xAxis)
     .attr("transform", `translate(0, ${innerHeight})`)
-      .select('.domain')
+
+    xAxisG
+    .select('.domain')
   .remove()
+
+  xAxisG.append('text').attr('y', -10).attr('x', innerWidth/2).attr('fill', 'black').text('cases')
 
   g.selectAll("rect")
     .data(data)
