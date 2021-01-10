a19197b2eaaef711b52ff5f91c3b7aba5abc2bf1
colmfah <colmdfahey@gmail.com>
2021-01-10 10:00:41 +0000

    xaxis stretches to end of data

--- scripts.js
+++ scripts.js
@@ -80,7 +80,7 @@ function setBarColor(data){
     const xScale = d3
         .scaleLinear()
         .domain([0, d3.max(data, (d) => d[metric])])
-        .range([30, innerWidth]);
+        .range([30, width]);
 
     const yScale = d3
         .scaleBand()
